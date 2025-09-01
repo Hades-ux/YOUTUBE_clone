cd49d15c34be740db0f8f4084e33d2c4cc2f45f9
Hades <vishal1807singh@gmail.com>
2025-09-01 12:51:50 +0530

    Add unsubscribe functionality: implement unSubscribeUser controller to handle user unsubscriptions and update subscriber counts.

--- Backend/src/controllers/subscriptionController.js
+++ Backend/src/controllers/subscriptionController.js
@@ -1,4 +1,6 @@
 import {User} from "../models/User.js"
+
+
 const subscribeUser = async (req, res) =>{
 
     const { channelId } = req.params;  // Id of user to subscibe(string from url)
@@ -46,4 +48,51 @@ const subscribeUser = async (req, res) =>{
     }
 }
 
-export { subscribeUser }
+const unSubscribeUser = async (req, res) =>{
+
+    const { channelId } = req.params;  // Id of user to unSubscibe(string from url)
+    const userId = req.user._id;
+
+    if(userId.toString() === channelId ){
+        return res.status(400).json({ error: "You cannot unsubscribe from yourself" });
+    }
+
+    try {
+
+        const channel = await User.findById(channelId)
+
+        if (!channel) {
+            return res.status(404).json({ error: "Channel not found" });
+        }
+
+        const isSubscribed = await User.exists({
+            _id: userId,
+            subscriptions: channelId
+        });
+
+       if (!isSubscribed) {
+           return res.status(400).json({ error: "You are not subscribed to this channel" });
+       }
+
+        // Remove channelId from user's subscriptions
+
+       await User.findByIdAndUpdate(userId, {
+           $pull: { subscriptions: channelId }
+       }, { new: true, validateBeforeSave: false });
+
+       //  Remove userId from channel's subscribers
+
+       await User.findByIdAndUpdate(channelId, {
+           $pull: { subscribers: userId },
+           $inc: { subscriberCount: -1 },
+       }, { new: true, validateBeforeSave: false });
+
+       return res.status(200).json({ message: "Unsubscribed successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message || "Internal server error" })
+
+    }
+}
+
+export { subscribeUser, unSubscribeUser }
